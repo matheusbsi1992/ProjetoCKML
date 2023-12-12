a77e8c23f12fde8dae11ff8105c8957464429162
Matheus Andrade <matheusbsi1992@gmail.com>
2023-12-12 15:46:55 -0300

    Teste de unidade sobre avaliacao estatica de metricas chidamker-kemerer com aprendizagem de maquina

--- src/main/java/org/com/projetock/Main.java
+++ src/main/java/org/com/projetock/Main.java
@@ -1,7 +1,6 @@
 package org.com.projetock;
 
 
-import org.com.projetock.chidamberkemerer.metricas.Processamento;
 import weka.classifiers.Evaluation;
 import weka.core.Instances;
 
@@ -9,33 +8,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.com.projetock.chidamberkemerer.metricas.Processamento.*;
+
 public class Main {
 
     public static void resultEvalution()  {
 
         try {
-            Instances[] instances = Processamento.divisaoTreinoeTeste(Processamento.dataInstancia(), 30);
+            Instances[] instances = divisaoTreinoeTeste(dataInstancia(), 30);
             Instances treino = instances[0];
             Instances teste = instances[1];
 
-            for (int i = 0; i < Processamento.classificadoresWeka().length; i++) {
+            for (int i = 0; i < classificadoresWeka().length; i++) {
 
-               Evaluation evaluation = Processamento.classificao(Processamento.classificadoresWeka()[i], treino, teste,10);
+               Evaluation evaluation = classificao(classificadoresWeka()[i], treino, teste,10);
 
                 System.out.println("*******************************************");
-                System.out.println(Processamento.classificadoresWeka()[i].getClass().getSimpleName());
+                System.out.println(classificadoresWeka()[i].getClass().getSimpleName());
                 System.out.println("*******************************************\n");
 
 
                 // Mapear resultados de volta para exemplos de dados reais
-                Map<String, Set<Integer>> uotestedMapping = Processamento.createUOTESTEDMapping();
-                Map<Integer, List<Double>> resultados = Processamento.evaluationResultados(evaluation);
+                Map<String, Set<Integer>> uotestedMapping   = createUOTESTEDMapping();
+                Map<Integer, List<Double>> resultados       = evaluationResultados(evaluation);
 
                 for (Map.Entry<Integer, List<Double>> entry : resultados.entrySet()) {
-                    int classeIndex = entry.getKey();
+                    int classeIndex      = entry.getKey();
                     List<Double> valores = entry.getValue();
 
-                    String classeValue = Processamento.mapIndexToCLASSE(classeIndex); // Mapear índice para valor de CLASSE
+                    String classeValue          = mapIndexToCLASSE(classeIndex); // Mapear índice para valor de CLASSE
                     Set<Integer> uotestedValues = uotestedMapping.get(classeValue); // Obter valor correspondente de UOTESTED
 
                     for (Integer value : uotestedValues) {
@@ -45,11 +46,11 @@ public class Main {
                         System.out.println("Falsos Positivos (FP): " + valores.get(2));
                         System.out.println("Falsos Negativos (FN): " + valores.get(3));
                         System.out.println("Área sob a Curva ROC: " + String.format("%.2f%%",valores.get(4)*100));
-                        System.out.println("Acurácia: "+String.format("%.2f%%", Processamento.acuracia(valores)*100));
-                        System.out.println("Precisão: "+String.format("%.2f%%", Processamento.precisao(valores)*100));
-                        System.out.println("Recall: "+String.format("%.2f%%", Processamento.recall(valores)*100));
-                        System.out.println("fMedida: "+String.format("%.2f%%", Processamento.fMedida(valores)*100));
-                        System.out.println("MCC: "+String.format("%.2f%%", Processamento.mcc(valores)*100));
+                        System.out.println("Acurácia: "+String.format("%.2f%%", acuracia(valores)*100));
+                        System.out.println("Precisão: "+String.format("%.2f%%", precisao(valores)*100));
+                        System.out.println("Recall: "+String.format("%.2f%%", recall(valores)*100));
+                        System.out.println("fMedida: "+String.format("%.2f%%", fMedida(valores)*100));
+                        System.out.println("MCC: "+String.format("%.2f%%", mcc(valores)*100));
                         System.out.println("UOTESTED correspondente: " +value);
                         System.out.println("-----------------------------------------------------\n");
                     }
